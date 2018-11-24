0c9a5129595233e83a451ac0a5a8ae0e0e9ad3c6
Matt Bettinson <mattbettinson@gmail.com>
2018-11-24 16:24:35 -0500

    Refactor positions of functions

--- lib/progression.rb
+++ lib/progression.rb
@@ -14,18 +14,14 @@ class Progression
 
     if ticks_per_bump > 1
       tick_more_elements_than_bars(ticks_per_bump)
-    # More bars than elements
     else
       tick_more_bars_than_elements(ticks_per_bump)
     end
   end
 
   private
-  attr_reader :size, :count
 
-  def window_size
-    @size ||= `tput cols`.strip.to_i
-  end
+  attr_reader :size, :count
 
   def tick_more_elements_than_bars(ticks_per_bump)
     if @current_inner_tick >= ticks_per_bump
@@ -36,9 +32,17 @@ class Progression
   end
 
   def tick_more_bars_than_elements(ticks_per_bump)
-    (window_size/count).to_i.times do
+    bump_size.times do
       print tick_character
     end
+  end
+
+  def window_size
+    @size ||= `tput cols`.strip.to_i
+  end
+
+  def bump_size
+    (window_size/count).to_i
   end
 
   def tick_character
